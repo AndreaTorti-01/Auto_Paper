48ba7f478fe4d56227774166e47f2ab935b84477
AndreaTorti-01 <eliandrea.yt@gmail.com>
2022-02-24 22:26:16 +0100

    build file updated

---
+++ build.py
@@ -0,0 +1,24 @@
+import PyInstaller.__main__
+import os
+import shutil
+
+PyInstaller.__main__.run([
+    'Auto_Paper.py',
+    '--onefile'
+])
+
+shutil.move("dist\Auto_Paper.exe", "Auto_Paper.exe")
+
+os.remove("Auto_Paper.spec")
+
+try:
+    shutil.rmtree("build")
+
+except OSError as e:
+    print("Error: %s - %s." % (e.filename, e.strerror))
+
+try:
+    shutil.rmtree("dist")
+
+except OSError as e:
+    print("Error: %s - %s." % (e.filename, e.strerror))
